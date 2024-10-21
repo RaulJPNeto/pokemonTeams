4bca42c2549ff5bfdefc63a3e767eade523413a6
raul <rauljose280@gmail.com>
2024-10-20 21:57:04 -0300

    Adicionado todo o Fluxo de criação de usuario e login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,45 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 exports.createUser = async (req, res) => {
     const {name, email, password} = req.body;
     try {
-        const user = new User({name, email, password});
+        let user = await User.findOne({ email });
+        if(user){
+            return res.status(400).json({message:"User already exists"});
+        }
+        user = new User({
+            name,
+            email,
+            password: bcrypt.hashSync(password, 10)
+        });
         await user.save();
-        res.status(201).json(user);
+        res.status(201).json({ msg: "User created successfully" });
     } catch (error) {
-        res.status(500).json({error: error.message});
+        console.log(error)
+        res.status(500).json({ msg: "Error creating user" });
+    }
+}
+
+exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(400).json({message:"User does not exist"});
+        }
+
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message:"Password does not match"});
+        }
+        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {expiresIn: "1h"});
+
+        res.status(201).json({msg:"User login successfully", token});
+    } catch (error) {
+        res.status(500).json({ msg: "Error logining user" });
     }
 }
