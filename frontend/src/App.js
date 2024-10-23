2d4a6c2dbbf83d5a527e968b9f38af27aa94e388
raul <rauljose280@gmail.com>
2024-10-23 07:52:12 -0300

    adicionado busca das regions na api e dos pokemons pertencentes a ela.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Register from './components/accounts/Register';
 import Login from './components/accounts/Login';
 import Dashboard from './components/Dashboard';
 import SelectRegion from './components/pokeTeam/SelectRegion';
+import SelectPokemon from './components/pokeTeam/SelectPokemon';
 import Navbar from './components/nav/Navbar';
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
               <Route path="/register" element={<Register />} />
               <Route path="/dashboard" element={isAuthenticated ? <Dashboard setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />}/>
               <Route path="/region" element={<SelectRegion/>} />
+              <Route path="/select-pokemon/:regionName" element={<SelectPokemon/>} />
               <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
       </Router>
